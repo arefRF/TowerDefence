6228d06605c79e5f019d671da523e6013eabd5ab
arf <valipour.aref@gmail.com>
2021-02-07 11:37:30 +0330

    tile and map files created

---
+++ Assets/Map/TileBase.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBase : MonoBehaviour
+{
+    
+}
+
+public class Tile_Road
+{
+
+}
+
+public class Tile_Crystal
+{
+
+}
+
+public class Tile_Goal
+{
+
+}
+
+public class Tile_Ground
+{
+
+}
