06613707d6f153d8a58c0405b1bb04ee1595e098
arf <valipour.aref@gmail.com>
2021-03-09 20:07:48 +0330

    minor changes

--- Assets/Enemy/EnemySpawner.cs
+++ Assets/Enemy/EnemySpawner.cs
@@ -10,16 +10,18 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private GameObject enemy_parent_;
     [SerializeField]
-    private TileBase spawner_point_;
+    private List<TileBase> spawner_point_;
     [SerializeField]
     private float spawn_interval_;
 
     private float current_interval_;
-    private NodePoint spawner_node_;
+    private List<NodePoint> spawner_node_;
     void Start()
     {
         current_interval_ = spawn_interval_;
-        spawner_node_ = spawner_point_.GetComponent<NodePoint>();
+        spawner_node_ = new List<NodePoint>();
+        for(int i=0; i<spawner_point_.Count; i++)
+            spawner_node_.Add(spawner_point_[i].GetComponent<NodePoint>());
     }
 
     // Update is called once per frame
@@ -35,8 +37,9 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnEnemy()
     {
-        var go = Instantiate(GetRandomEnemy(), spawner_point_.transform.position, spawner_point_.transform.rotation, enemy_parent_.transform);
-        go.GetComponent<MoveTest>().destination_ = spawner_node_;
+        var rand = Random.Range(0, spawner_point_.Count);
+        var go = Instantiate(GetRandomEnemy(), spawner_point_[rand].transform.position, spawner_point_[rand].transform.rotation, enemy_parent_.transform);
+        go.GetComponent<MoveTest>().destination_ = spawner_node_[rand];
         EnemyManager.sSingletone.AddEnemyToList(go.GetComponent<EnemyBase>());
     }
 
